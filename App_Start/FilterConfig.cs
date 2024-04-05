0c5395181413257a2528f90911daed4044b94abe
Wen-Hsin Chen <chenwenhsin0@gmail.com>
2024-04-04 22:04:33 -0700

    Added all files except those .vs/FileContentIndex/

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Group5__iCLOTHINGApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
